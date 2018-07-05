3728d0a760e2d41fce659f0f253b85f2b5f699e8
ishigo1987 <stephnguets@yahoo.fr>
2018-07-05 12:35:15 +0200

    Test

--- src/views/home.js
+++ src/views/home.js
@@ -12,10 +12,12 @@ exports.create = ()=>{
                    const j = requestResult.length;
                  console.log(requestResult);
                  console.log(j);
-                 console.log(requestResult.item(0).name);
+                 
                    let campagnesArray = [];
                    for(let i=0; i<j; i++){
-                     campagnesArray.push({campaignId:requestResult.item(i).id,campaignName:requestResult.item(i).name,campaignPassword:requestResult.item(i).password_plan,campaignPriceFirstLeaf:RequestResult.item(i).prix_feuille1,campaignPriceSecondLeaf:requestResult.item(i).prix_feuille2,campaignPriceThirdLeaf:requestResult.item(i).prix_feuille3,campaignPriceXLeaf:requestResult.item(i).prix_feuille_x,campaignPriceCutLeaf:requestResult.item(i).prix_coupe,campaignTauxRefraction:requestResult.item(i).taux_refraction,campaignPriceTriage:requestResult.item(i).prix_triage});   
+                     console.log(requestResult.item(i).name);
+                       console.log(requestResult.item(i).taux_refraction);
+                    campagnesArray.push({campaignId:requestResult.item(i).id,campaignName:requestResult.item(i).name,campaignPassword:requestResult.item(i).password_plan,campaignPriceFirstLeaf:RequestResult.item(i).prix_feuille1,campaignPriceSecondLeaf:requestResult.item(i).prix_feuille2,campaignPriceThirdLeaf:requestResult.item(i).prix_feuille3,campaignPriceXLeaf:requestResult.item(i).prix_feuille_x,campaignPriceCutLeaf:requestResult.item(i).prix_coupe,campaignTauxRefraction:requestResult.item(i).taux_refraction,campaignPriceTriage:requestResult.item(i).prix_triage});   
                    }
                    console.log(campagnesArray);
                    const scrollView = new ScrollView({top:0,right:0,left:0,bottom:0}).appendTo(homePage);
