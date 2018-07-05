2deed7a6566e456f94bd534e22567268b8db9310
ishigo1987 <stephnguets@yahoo.fr>
2018-07-05 10:32:36 +0200

    test

--- src/modules/retrieveDataCampaignToDatabase.js
+++ src/modules/retrieveDataCampaignToDatabase.js
@@ -6,6 +6,7 @@ module.exports = ()=>{
          db = window.sqlitePlugin.openDatabase({name: 'MctDataBase', location: 'default'},success,failure);
          db.transaction((tx)=>{
            tx.executeSql('SELECT * FROM campagnes', [],(tx, rs)=>{
+               console.log(String(rs));
              resolve({Message:'Data retrieves with success',requestResult:rs.rows.item(),rowLength:rs.rows.length});
            },(tx, error)=>{
               console.log('SELECT error: ' + error.message);
